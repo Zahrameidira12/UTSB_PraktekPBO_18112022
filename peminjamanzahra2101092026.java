3ca7b451e924c9440f9bc70a06b0dd5861d4135f
Zahrameidira12 <114121984+Zahrameidira12@users.noreply.github.com>
2022-11-18 13:35:52 +0700

    Add files via upload

--- peminjamanzahra2101092026.java
+++ peminjamanzahra2101092026.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UTS.B_PraktekPBO_18112022;
+package UTSB_PraktekPBO_18112022;
 
 /**
  *
@@ -14,62 +14,94 @@ public class peminjamanzahra2101092026 {
        private int kodepeminjaman ; 
        private  int jumlahpeminjaman;
        private  int kodenasabah; 
-       private String namanasbah; 
+       private  int lamapeminjaman;
+       private String namanasabah; 
        private  String tanggal;
        private double bunga;
-       private  int angsuranpokok;
-       private  int lamapeminjaman;
-       private  int total;
-       private int angsuranperbulan;
-       private  int bungab;
-       
+       private  double angsuranpokok; 
+       private double angsuranperbulan;
+       private  double total;       
        
        
        private static int peminjamanzahra2101092026;
+        public int getKodepeminjaman(){ 
+            return kodepeminjaman; 
+        }
+        
+         public void setKodepeminjaman( int kodepeminjaman ){ 
+             this. kodepeminjaman = kodepeminjaman; 
+        }
        
+       
+        public int getJumlahpeminjaman(){ 
+            return jumlahpeminjaman; 
+        }
+        
+         public void setJumlahpeminjaman( int jumlahpeminjaman ){ 
+             this. jumlahpeminjaman = jumlahpeminjaman; 
+        }
+        
+        
+        public int getKodenasabah(){ 
+            return kodenasabah; 
+        }
+         
+        public void setKodenasabah( int kodenasabah ){ 
+             this. kodenasabah = kodenasabah; 
+        }
+        
+                
+        public int getLamapeminjaman(){ 
+            return lamapeminjaman; 
+        }
+            
+        public void setLamapeminjaman( int lamapeminjaman ){ 
+             this. lamapeminjaman = lamapeminjaman; 
+        }
+        
        public String getNamanasabah(){ 
-            return namanasbah; 
+            return namanasabah; 
+        } 
+       public void setNamanasabah( String namanasabah ){ 
+             this. namanasabah = namanasabah; 
         } 
        
        public String getTanggal(){ 
             return tanggal; 
         } 
+       public void setTanggal( String tanggal ){ 
+             this. tanggal = tanggal; 
+        } 
+      
        
-        public int getKodepeminjaman(){ 
-            return kodepeminjaman; 
-        }
-        
-        public int getJumlahpeminjaman(){ 
-            return jumlahpeminjaman; 
-        }
-        
-        public int getKodenasabah(){ 
-            return kodenasabah; 
-        }
-        
-        public int getAngsuranpokok(){ 
+       public double getBunga() { 
+            bunga = ( 0.12 *jumlahpeminjaman);
+            return bunga; 
+        } 
+       
+       public double getAngsuranpokok() { 
+            angsuranpokok = ( jumlahpeminjaman/lamapeminjaman);
             return angsuranpokok; 
         }
-        
-        
-       public int getLamapeminjaman(){ 
-            return lamapeminjaman; 
-        }
        
-       public int getTotal(){ 
-            return total; 
-        }
-       
-       public int getAngsuranperbulan(){ 
+       public double getAngsuranperbulan(){ 
+            angsuranperbulan = bunga + angsuranpokok;
             return angsuranperbulan; 
         }
        
-       
-       public double getBungab(){ 
-            return bungab; 
-        } 
-       
-       
-       
-    
+       public double getTotal(){ 
+            total = jumlahpeminjaman + bunga;
+            return total; 
+        }
+   
+        /*angsuranpokok=jumlahpeminjaman/lamapeminjaman;
+        bunga= 0.12 *jumlahpeminjaman;
+        bungab= bunga / lamapeminjaman;
+        angsuranperbulan = bungab + angsuranpokok;
+        total = jumlahpeminjaman + bunga;
+        */
 }
+
+    
+
+       
--- peminjamanzahrarecord2101092026.java
+++ peminjamanzahrarecord2101092026.java
@@ -3,12 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UTS.B_PraktekPBO_18112022;
+package UTSB_PraktekPBO_18112022;
+
+import zahra041122.StudentRecord;
 
 /**
  *
  * @author HP
  */
-public class peminjamanzahrarecord {
-    
+public class peminjamanzahrarecord2101092026 {
+    public static void main (String[] args) {
+        peminjamanzahra2101092026 nasabah1 = new peminjamanzahra2101092026();
+      
+        nasabah1.setKodepeminjaman(001);
+        nasabah1.setKodenasabah(20);
+        nasabah1.setJumlahpeminjaman(100000);
+        nasabah1.setLamapeminjaman(1);
+        nasabah1.setTanggal("18 November 2022");
+  
+        System.out.println("Kode Peminjaman   :"+nasabah1.getKodepeminjaman());
+        System.out.println("Kode Nasabah      :"+nasabah1.getKodenasabah());
+        System.out.println("Tanggal           :"+nasabah1.getTanggal());
+        System.out.println("Jumlah Peminjaman :"+nasabah1.getJumlahpeminjaman());
+        System.out.println("Lama Peminjaman   :"+nasabah1.getLamapeminjaman());
+        System.out.println("Angsuranpokok     :"+nasabah1.getAngsuranpokok());
+        System.out.println("Bunga             :"+nasabah1.getBunga());
+        System.out.println("Angsuran perbulan :"+nasabah1.getAngsuranperbulan());
+        System.out.println("Total Hutang      :"+nasabah1.getTotal());  
+    }
 }
