15f18ac6c54d13d702da0a62ddfeb123d08667dc
Zahrameidira12 <114121984+Zahrameidira12@users.noreply.github.com>
2022-11-18 12:03:47 +0700

    Add files via upload

---
+++ peminjamanzahra2101092026.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS.B_PraktekPBO_18112022;
+
+/**
+ *
+ * @author HP
+ */
+public class peminjamanzahra2101092026 {
+    
+       private int kodepeminjaman ; 
+       private  int jumlahpeminjaman;
+       private  int kodenasabah; 
+       private String namanasbah; 
+       private  String tanggal;
+       private double bunga;
+       private  int angsuranpokok;
+       private  int lamapeminjaman;
+       private  int total;
+       private int angsuranperbulan;
+       private  int bungab;
+       
+       
+       
+       private static int peminjamanzahra2101092026;
+       
+       public String getNamanasabah(){ 
+            return namanasbah; 
+        } 
+       
+       public String getTanggal(){ 
+            return tanggal; 
+        } 
+       
+        public int getKodepeminjaman(){ 
+            return kodepeminjaman; 
+        }
+        
+        public int getJumlahpeminjaman(){ 
+            return jumlahpeminjaman; 
+        }
+        
+        public int getKodenasabah(){ 
+            return kodenasabah; 
+        }
+        
+        public int getAngsuranpokok(){ 
+            return angsuranpokok; 
+        }
+        
+        
+       public int getLamapeminjaman(){ 
+            return lamapeminjaman; 
+        }
+       
+       public int getTotal(){ 
+            return total; 
+        }
+       
+       public int getAngsuranperbulan(){ 
+            return angsuranperbulan; 
+        }
+       
+       
+       public double getBungab(){ 
+            return bungab; 
+        } 
+       
+       
+       
+    
+}
